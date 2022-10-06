8a023ee86212b48fcfc4b7d59a8e4db2372a5ede
Colborne <Logan.Colborne@gmail.com>
2022-10-05 23:38:17 -0400

    RAGDOLL

---
+++ Assets/DamageCollider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageCollider : MonoBehaviour
+{
+    public Collider damageCollider;
+    public float force;
+    private void Awake() 
+    {
+        damageCollider.gameObject.SetActive(true);
+        damageCollider.isTrigger = true;
+        damageCollider.enabled = false;
+    }
+
+    public void EnableDamageCollider()
+    {
+        damageCollider.enabled = true;
+    }
+
+    public void DisableDamageCollider()
+    {
+        damageCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider collision) 
+    {
+        if(collision.tag == "Enemy")
+        {
+            Rigidbody[] bodies = collision.GetComponentsInChildren<Rigidbody>();
+            foreach (Rigidbody _rb in bodies)
+            {
+                //_rb.AddExplosionForce(15, transform.position, 25f, 0f, ForceMode.Impulse);
+                _rb.AddForce(transform.forward * force);
+            }
+            RagdollController rdc = collision.GetComponent<RagdollController>();
+            Rigidbody rb = collision.GetComponent<Rigidbody>();
+            //rb.AddForce(transform.forward * force);
+            rdc.EnableRagdoll();
+        }
+    }
+}
---
+++ Assets/RagdollController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollController : MonoBehaviour
+{
+
+    void Start()
+    {
+        SetRigidBodyState(true);
+        SetColliderState(false);
+    }
+
+    public void EnableRagdoll()
+    {
+        GetComponent<Animator>().enabled = false;
+        SetRigidBodyState(false);
+        SetColliderState(true);
+    }
+
+    public void SetRigidBodyState(bool state)
+    {
+        Rigidbody[] bodies = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rb in bodies)
+        {
+            rb.isKinematic = state;
+        }
+        GetComponent<Rigidbody>().isKinematic = !state;
+    }
+
+    public void SetColliderState(bool state)
+    {
+        Collider[] bodies = GetComponentsInChildren<Collider>();
+        foreach (Collider rb in bodies)
+        {
+            rb.enabled = state;
+        }
+        GetComponent<Collider>().enabled = !state;
+    }
+}
